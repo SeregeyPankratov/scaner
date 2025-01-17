9b09d2a85deee41d058c9700253f3c1a8abdb21b
Sergey.Pankratov <sspankratovss@gmail.com>
2025-01-17 17:25:59 +0500

    delete button

--- src/App.vue
+++ src/App.vue
@@ -5,94 +5,10 @@
       class="mx-auto"
       max-width="600"
     >
-      <AppMenu 
-        @show-qr-scanner="showQRScanner()"  
-        @show-history="show_history = true"
-        @show-settings="show_history = false"          
-      />
-      <!--history-->
-      <v-card 
-        v-if="show_history"
-      >
-        <div 
-          v-if="!cloud_storage_keys.length"
-          class="text-center headline mb-4 mt-4"
-        >
-          Scan a QR code!
-        </div>
-        <v-expansion-panels
-          v-if="cloud_storage_keys.length"
-          v-model="expanded_panels"
-          class="mt-4"
-        >
-          <v-expansion-panel
-            v-for="(akey, index) in cloud_storage_keys"
-            :key="index"
-          >
-            <v-expansion-panel-title>
-              <v-row no-gutters>
-                <v-col
-                  cols="2" 
-                  class="d-flex justify-start"
-                >
-                  <v-avatar color="grey-lighten-1">
-                    <v-icon color="white">
-                      {{ getIconFromType(akey) }}
-                    </v-icon>
-                  </v-avatar>
-                </v-col>
-                <v-col
-                  cols="10"
-                >
-                  <div>
-                    <div 
-                      class="headline mb-1"
-                    >
-                      {{ limitLength(cloud_storage_values[akey], 35) }}
-                    </div>
-                    <div
-                      class="text-subtitle-2 text-grey"
-                    >
-                      {{ formattedDate(akey) }}
-                    </div>
-                  </div>
-                </v-col>
-              </v-row>
-            </v-expansion-panel-title>
-            <v-expansion-panel-text 
-              v-if="enriched_values[akey] && enriched_values[akey].hasOwnProperty('type')"
-            >
-              <CardGeo
-                v-if="enriched_values[akey]['type'] === 'geo'"
-                :data="enriched_values[akey]['info']"
-                @remove-key="removeKey(akey)"  
-              />
-              <CardUrl
-                v-if="enriched_values[akey]['type'] === 'url'"
-                :data="enriched_values[akey]['info']"
-                @remove-key="removeKey(akey)" 
-              />
-              <CardWifi
-                v-if="enriched_values[akey]['type'] === 'wifi'"
-                :data="enriched_values[akey]['info']"
-                @remove-key="removeKey(akey)" 
-              />
-              <CardVCard
-                v-if="enriched_values[akey]['type'] === 'vcard'"
-                :data="enriched_values[akey]['info']"
-                @remove-key="removeKey(akey)" 
-              />
-              <CardText
-                v-if="enriched_values[akey]['type'] === 'text'"
-                :text="enriched_values[akey]['info']"
-                @remove-key="removeKey(akey)"
-              />
-            </v-expansion-panel-text>
-          </v-expansion-panel>
-        </v-expansion-panels>
-      </v-card>
+      <AppMenu @show-qr-scanner="showQRScanner()" />
+
       <!--settings-->
-      <AppSettings 
+      <AppSettings
         v-if="!show_history"
         :is-continuous-scan="is_continuous_scan"
         :cloud-storage-values="cloud_storage_values"
@@ -103,7 +19,7 @@
         @enrich-values="enrichValues(cloud_storage_values)"
       />
     </v-card>
-    <RequirementsMessage 
+    <RequirementsMessage
       :is-telegram-client="is_telegram_client"
       :is-telegram-api-updated="is_telegram_api_updated"
     />
@@ -111,15 +27,21 @@
 </template>
 
 <script>
-import { detectCodeType, prepareUrl, prepareCoordinate, prepareWifi, prepareVCard } from './helpers';
+import {
+  detectCodeType,
+  prepareUrl,
+  prepareCoordinate,
+  prepareWifi,
+  prepareVCard,
+} from "./helpers";
 import AppMenu from "./components/AppMenu.vue";
-import AppSettings from "./components/AppSettings.vue"
+import AppSettings from "./components/AppSettings.vue";
 import CardUrl from "./components/CardUrl.vue";
 import CardGeo from "./components/CardGeo.vue";
 import CardWifi from "./components/CardWifi.vue";
-import CardVCard from "./components/CardVCard.vue"
+import CardVCard from "./components/CardVCard.vue";
 import CardText from "./components/CardText.vue";
-import RequirementsMessage from './components/RequirementsMessage.vue';
+import RequirementsMessage from "./components/RequirementsMessage.vue";
 
 export default {
   components: {
@@ -130,7 +52,7 @@ export default {
     CardWifi,
     CardVCard,
     CardText,
-    RequirementsMessage
+    RequirementsMessage,
   },
   data() {
     return {
@@ -150,11 +72,11 @@ export default {
   created() {
     // Binding function to the events types
     this.TMA.MainButton.setText("Scan QR code");
-    this.TMA.onEvent('qrTextReceived', this.processQRCode);
-    this.TMA.onEvent('mainButtonClicked', this.mainButtonClicked);
+    this.TMA.onEvent("qrTextReceived", this.processQRCode);
+    this.TMA.onEvent("mainButtonClicked", this.mainButtonClicked);
 
     // platform not updated if version is not 6.9 or greater
-    this.is_telegram_api_updated = this.TMA.isVersionAtLeast('6.9');
+    this.is_telegram_api_updated = this.TMA.isVersionAtLeast("6.9");
     if (this.TMA.platform != "unknown") {
       this.is_telegram_client = true;
     }
@@ -204,18 +126,26 @@ export default {
     enrichValue(key) {
       this.enriched_values[key] = {};
       const code_type = detectCodeType(this.cloud_storage_values[key]);
-      this.enriched_values[key]['type'] = code_type;
-    
+      this.enriched_values[key]["type"] = code_type;
+
       if (code_type == "geo") {
-        this.enriched_values[key]['info'] = prepareCoordinate(this.cloud_storage_values[key]);
+        this.enriched_values[key]["info"] = prepareCoordinate(
+          this.cloud_storage_values[key]
+        );
       } else if (code_type == "wifi") {
-        this.enriched_values[key]['info'] = prepareWifi(this.cloud_storage_values[key]);
+        this.enriched_values[key]["info"] = prepareWifi(
+          this.cloud_storage_values[key]
+        );
       } else if (code_type == "vcard") {
-        this.enriched_values[key]['info'] = prepareVCard(this.cloud_storage_values[key]);
+        this.enriched_values[key]["info"] = prepareVCard(
+          this.cloud_storage_values[key]
+        );
       } else if (code_type == "url") {
-        this.enriched_values[key]['info'] = prepareUrl(this.cloud_storage_values[key]);
+        this.enriched_values[key]["info"] = prepareUrl(
+          this.cloud_storage_values[key]
+        );
       } else {
-        this.enriched_values[key]['info'] = this.cloud_storage_values[key];
+        this.enriched_values[key]["info"] = this.cloud_storage_values[key];
       }
     },
     enrichValues(data) {
@@ -240,10 +170,10 @@ export default {
     showQRScanner() {
       // Sets QR message
       let par = {
-          text: ""
-        };
+        text: "",
+      };
       if (this.is_continuous_scan) {
-        par['text'] = "Continuous scan enabled.";
+        par["text"] = "Continuous scan enabled.";
       }
       this.TMA.showScanQrPopup(par);
     },
@@ -251,7 +181,9 @@ export default {
       // This function is called every time the scanner recognise a QR code
       // check if the QR code text is longer than 4096 characters
       if (data.data.length > 4096) {
-        this.TWA.showAlert('Error cannot store QR codes longer than 4096 characters');
+        this.TWA.showAlert(
+          "Error cannot store QR codes longer than 4096 characters"
+        );
         return;
       }
       // avoids to scan the same code twice in continuous scan mode
@@ -290,7 +222,11 @@ export default {
       const minute = date.getMinutes();
       const second = date.getSeconds();
       // Format the date as "dd/mm/yyyy hh:mm:ss"
-      const formattedDate = `${day.toString().padStart(2, '0')}/${month.toString().padStart(2, '0')}/${year} ${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}:${second.toString().padStart(2, '0')}`;
+      const formattedDate = `${day.toString().padStart(2, "0")}/${month
+        .toString()
+        .padStart(2, "0")}/${year} ${hour.toString().padStart(2, "0")}:${minute
+        .toString()
+        .padStart(2, "0")}:${second.toString().padStart(2, "0")}`;
       return formattedDate;
     },
     getIconFromType(key) {
@@ -299,10 +235,10 @@ export default {
         return "mdi-text-box";
       }
       // check it key type exists
-      if (!this.enriched_values[key]['type']) {
+      if (!this.enriched_values[key]["type"]) {
         return "mdi-text-box";
       }
-      let type = this.enriched_values[key]['type'];
+      let type = this.enriched_values[key]["type"];
       if (type == "geo") {
         return "mdi-map-marker-outline";
       } else if (type == "wifi") {
@@ -320,16 +256,16 @@ export default {
         return value;
       }
       return value.substring(0, max_length) + "...";
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style scoped>
-  #main {
-    background-color: var(--tg-theme-bg-color, white);
-    color: var(--tg-theme-text-color, black);
-    /*https://stackoverflow.com/questions/1165497/how-to-prevent-text-from-overflowing-in-css*/
-    word-wrap: break-word;
-  }
+#main {
+  background-color: var(--tg-theme-bg-color, white);
+  color: var(--tg-theme-text-color, black);
+  /*https://stackoverflow.com/questions/1165497/how-to-prevent-text-from-overflowing-in-css*/
+  word-wrap: break-word;
+}
 </style>
